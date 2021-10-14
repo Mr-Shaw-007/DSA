85521775716c44c0e543d1315035b2e7a7609bc5
Ankit Kumar Shaw <ankitshaw.shaw97@gmail.com>
2021-10-14 20:17:29 +0530

    Normal

---
+++ Array/Array_ADT.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Array{
+    int *A;
+    int size;
+    int length;
+};
+
+
+int main(){
+
+    struct Array arr;
+    cout<<"Emter The Size of an Array  ";
+    cin>> arr.size;
+    arr.length=0;
+
+    int n;
+
+    cout<<"Enter The No. Of Elements To be Stored in array : ";
+    cin>>n;
+
+    arr.A= new int[n];
+
+    
+
+
+
+
+
+}
